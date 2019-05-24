ba6fae8aede610f7ee7442a51d3ab3f3f013f1ed
Niklas Baumbach <Niklas.Baumbach@dlr.de>
2019-05-24 10:56:38 +0200

    added pluginbase package to dynamically load algorithms

--- test_env/environment.py
+++ test_env/environment.py
@@ -1,10 +1,23 @@
+from pluginbase import PluginBase
 import datasets
 
-DATASET_PATH = '..\\datasets\\'
-ALGORITHM_PATH = '..\\algorithms\\'
+DATASET_PATH = '../datasets/'
+ALGORITHM_PATH = 'algorithms/'
 
 if __name__ == '__main__':
-    print('Loading all datasets from given directory...', end='')
+    print('Loading all available datasets...', end='', flush=True)
     ds_manager = datasets.DatasetManager(DATASET_PATH)
     print('Success')
+
+    print('Loading all available algorithms...', end='', flush=True)
+    algorithm_base = PluginBase(package='testenv.algorithms')
+    algorithm_source = algorithm_base.make_plugin_source(search_path=[ALGORITHM_PATH])
+
+    algorithms={}
+
+    for algorithm in algorithm_source.list_plugins():
+        print(algorithm)
+
+    print('Success')
+
     import code; code.interact(local=locals())
