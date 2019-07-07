7a521e565f9f7ac0705bb39540a668c87fda11f4
Martin Immel <martinimmeluni@gmail.com>
2019-07-07 19:20:55 +0200

    Added new functions to plot with plotly.

--- fdca/visualisation_new.py
+++ fdca/visualisation_new.py
@@ -1,20 +1,110 @@
-import plotly as py
+import plotly.plotly as py
 import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
-N = 1000
-random_x = np.random.randn(N)
-random_y = np.random.randn(N)
 
-# Create a trace
-trace = go.Scatter(
-    x = random_x,
-    y = random_y,
-    mode = 'markers'
-)
+def plot_line(array):
+    trace = go.Scatter(
+        y = array,
+        mode = "lines"
+    )
 
-data = [trace]
+    data = [trace]
 
-# Plot and embed in ipython notebook!
-py.plotly.iplot(data, filename='basic-scatter')
+    py.plot(data, filename="basic-line", auto_open=True)
+
+def plot_2D(store, x="x", y="y"):
+    traces = []
+
+    for i in store.cz:
+        temp_cluster_array = store.df[(store.df["cluster_center"] == i) & (store.df["cluster_center"] != store.df.index)]
+
+        traces.append(
+            go.Scatter(
+                x = temp_cluster_array[x],
+                y = temp_cluster_array[y],
+                name = "Cluster " + str(i),
+                mode = 'markers'
+            )
+        )
+
+    cluster_centers = store.df[store.df["cluster_center"] == store.df.index]
+
+    traces.append(
+        go.Scatter(
+            x = cluster_centers[x],
+            y = cluster_centers[y],
+            name = "Cluster Centers",
+            mode = "markers",
+            marker = dict(
+                color = "rgb(0, 0, 0)"
+            )
+        )
+    )
+
+    data = traces
+
+    layout= go.Layout(
+        # title= 'Clustering',
+        xaxis= dict(
+            title= x
+        ),
+        yaxis=dict(
+            title= y
+        )
+    )
+
+    figure = go.Figure(data=data, layout=layout)
+
+    # Plot
+    py.plot(figure, filename='basic-scatter', auto_open=True)
+
+def plot_3D(store, x="x", y="y", z="z"):
+    traces = []
+
+    for i in store.cz:
+        temp_cluster_array = store.df[(store.df["cluster_center"] == i) & (store.df["cluster_center"] != store.df.index)]
+
+        # Create a trace
+        traces.append(
+            go.Scatter3d(
+                x = temp_cluster_array[x],
+                y = temp_cluster_array[y],
+                z = temp_cluster_array[z],
+                name = "Cluster " + str(i),
+                mode = 'markers'
+            )
+        )
+
+    cluster_centers = store.df[store.df["cluster_center"] == store.df.index]
+
+    traces.append(
+        go.Scatter3d(
+            x = cluster_centers[x],
+            y = cluster_centers[y],
+            z = cluster_centers[z],
+            name = "Cluster Centers",
+            mode = "markers",
+            marker = dict(
+                color = "rgb(0, 0, 0)"
+            )
+        )
+    )
+
+    data = traces
+
+    layout= go.Layout(
+        # title= 'Clustering',
+        margin = dict(
+            l = 0,
+            r = 0,
+            b = 0,
+            t = 0
+        )
+    )
+
+    figure = go.Figure(data=data, layout=layout)
+
+    # Plot
+    py.plot(figure, filename='basic-3d-scatter', auto_open=True)
