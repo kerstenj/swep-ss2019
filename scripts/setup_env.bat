05d819a1de91840305868fe4675de449268c5615
Martin Immel <martinimmeluni@gmail.com>
2019-07-09 14:32:25 +0200

    Correct function call

--- scripts/setup_env.bat
+++
@@ -1,3 +0,0 @@
-pip install virtualenv
-virtualenv ..\env
-cmd /k "..\env\scripts\activate & pip install -r ..\requirements.in & deactivate"
--- scripts/setup_env.sh
+++
@@ -1,5 +0,0 @@
-# pip install virtualenv
-virtualenv -p python3 ../env
-source ../env/bin/activate
-pip install -r ../requirements.in
-deactivate
